6209a44f0d629c11ba5889d8b025f557511deede
Вадим <exth331@gmail.com>
2026-05-21 05:41:04 +0300

    initial com

---
+++ dataStorage/dataStorage/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  dataStorage
+//
+//  Created by Вадим on 21.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("aa")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dataStorage/dataStorage/dataStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  dataStorageApp.swift
+//  dataStorage
+//
+//  Created by Вадим on 21.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct dataStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
